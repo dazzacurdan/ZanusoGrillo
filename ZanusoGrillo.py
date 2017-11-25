b371be52f016f2561a4c1a7e585e1693c5262c0f
dazzacurdan <pasterispaolo@gmail.com>
2017-11-25 12:21:01 +0100

    fixed syntax bugs

--- ZanusoGrillo.py
+++ ZanusoGrillo.py
@@ -79,18 +79,18 @@ print('Press Ctrl-C to quit.')
 
 while True:
 
-    reading = GPIO.input(PIN_INPUT);
+    reading = GPIO.input(PIN_INPUT)
 
     if ((millis() - lastStateChangeTime) > dialHasFinishedRotatingAfterMs):
         # the dial isn't being dialed, or has just finished being dialed.
         if (needToPrint):
             # if it's only just finished being dialed, we need to send the number down the serial
             # line and reset the count. We mod the count by 10 because '0' will send 10 pulses.
-            targetProject += String(count % 10);
+            targetProject += String(count % 10)
             #Serial.print(count % 10, DEC);
 
             if(targetProject.length() == 2):
-                Serial.print(targetProject);
+                Serial.print(targetProject)
                 if( targetProject == "00" ):
                     path = videoPaths(i)
                     print( "/play " + path[0] )
@@ -114,25 +114,25 @@ while True:
                     #Keyboard.press('g');
                 #delay(10);
                 #Keyboard.releaseAll();
-                targetProject = "";
+                targetProject = ""
             
             client.send_message("/play", path[0] )
             threading.Thread(target=event_lock_holder, args=(lock,path[1]), name='eventLockHolder').start()
 
-            needToPrint = 0;
-            count = 0;
-            cleared = 0;
+            needToPrint = 0
+            count = 0
+            cleared = 0
 
     if (reading != lastState):
-        lastStateChangeTime = millis();
+        lastStateChangeTime = millis()
 
     if ((millis() - lastStateChangeTime) > debounceDelay):
         #debounce - this happens once it's stablized
         if (reading != trueState):
             # this means that the switch has either just gone from closed->open or vice versa.
-            trueState = reading;
+            trueState = reading
             if (trueState == HIGH):
                 # increment the count of pulses if it's gone high.
                 count++; 
                 needToPrint = 1; # we'll need to print this number (once the dial has finished rotating)
-    lastState = reading;
+    lastState = reading
