f8959c96779c454745b9c2a66574943cde036f21
dazzacurdan <pasterispaolo@gmail.com>
2017-11-25 12:14:24 +0100

    removed {

--- ZanusoGrillo.py
+++ ZanusoGrillo.py
@@ -22,7 +22,7 @@ cleared = 0;
 dialHasFinishedRotatingAfterMs = 100;
 debounceDelay = 10;
 
-String targetProject;
+targetProject;
 
 def event_lock_holder(lock,delay):
     print('Starting')
@@ -81,62 +81,40 @@ while True:
 
     reading = GPIO.input(PIN_INPUT);
 
-    if ((millis() - lastStateChangeTime) > dialHasFinishedRotatingAfterMs) {
+    if ((millis() - lastStateChangeTime) > dialHasFinishedRotatingAfterMs)
         # the dial isn't being dialed, or has just finished being dialed.
-        if (needToPrint) {
+        if (needToPrint)
             # if it's only just finished being dialed, we need to send the number down the serial
             # line and reset the count. We mod the count by 10 because '0' will send 10 pulses.
             targetProject += String(count % 10);
             #Serial.print(count % 10, DEC);
 
             if(targetProject.length() == 2)
-            {
                 Serial.print(targetProject);
                 if( targetProject == "00" )
-                {
                     path = videoPaths(i)
                     print( "/play " + path[0] )
-                }
                 if( targetProject == "01" )
-                {
                     #Keyboard.press('w');
-                }
                 if( targetProject == "02" )
-                {
                     #Keyboard.press('e');
-                }
                 if( targetProject == "03" )
-                {
                     #Keyboard.press('r');
-                }
                 if( targetProject == "04" )
-                {
                     #Keyboard.press('t');
-                }
                 if( targetProject == "05" )
-                {
                     #Keyboard.press('a');
-                }
                 if( targetProject == "06" )
-                {
                     #Keyboard.press('s');
-                }
                 if( targetProject == "07" )
-                {
                     #Keyboard.press('d');
-                }
                 if( targetProject == "08" )
-                {
                     #Keyboard.press('f');
-                }
                 if( targetProject == "09" )
-                {
                     #Keyboard.press('g');
-                }
                 #delay(10);
                 #Keyboard.releaseAll();
                 targetProject = "";
-            } 
             
             client.send_message("/play", path[0] )
             threading.Thread(target=event_lock_holder, args=(lock,path[1]), name='eventLockHolder').start()
@@ -144,23 +122,17 @@ while True:
             needToPrint = 0;
             count = 0;
             cleared = 0;
-        }
-    } 
 
-    if (reading != lastState) {
+    if (reading != lastState)
         lastStateChangeTime = millis();
-    }
 
-    if ((millis() - lastStateChangeTime) > debounceDelay) {
+    if ((millis() - lastStateChangeTime) > debounceDelay)
         #debounce - this happens once it's stablized
-        if (reading != trueState) {
+        if (reading != trueState)
             # this means that the switch has either just gone from closed->open or vice versa.
             trueState = reading;
-            if (trueState == HIGH) {
+            if (trueState == HIGH)
                 # increment the count of pulses if it's gone high.
                 count++; 
                 needToPrint = 1; # we'll need to print this number (once the dial has finished rotating)
-            } 
-        }
-    }
     lastState = reading;
